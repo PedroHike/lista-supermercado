028a89c297d01dd565bb6deaba04c1ca4e044807
PedroHike <127800407+PedroHike@users.noreply.github.com>
2023-04-21 13:28:01 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,89 @@
+var itens = [];
+
+
+function cadastrar() {
+    var nomeProduto = document.querySelector('input[name=nome-produto]');
+    var valorProduto = document.querySelector('input[name=valor-produto]');
+    var quantidade = document.querySelector('input[name=quantidade]');
+    var resultado = valorProduto.value * quantidade.value;
+    var resultadoArredondado = resultado.toFixed(2);
+
+    itens.push({
+        nome: nomeProduto.value,
+        valor: resultadoArredondado,
+    });
+
+    /*
+        <div class="lista-produto-single">
+            <h4 class="produto">redbul</h4>
+            <div class="valor">
+                <h4 class="valor">R$20.00</h4>
+                <button name="excluir">x</button>
+            </div>
+        </div>
+    */
+    let listaProdutos = document.querySelector('.lista-produtos')
+    let soma = 0;
+    listaProdutos.innerHTML = "";
+    itens.map((val) => {
+        soma += parseFloat(val.valor);
+
+        listaProdutos.innerHTML += `
+        <div class="lista-produto-single">
+            <h4 class="produto">`+ val.nome + `</h4>
+            <div class="valor">
+                <h4>R$<span class="price">`+ val.valor + `</span></h4>
+                <button class="excluir">x</button>
+            </div>
+        </div>
+        `;
+
+    itens.sort();
+    console.log(itens);
+        
+    } )
+
+    soma = soma.toFixed(2);
+    nomeProduto.value = "";
+    valorProduto.value = "";
+    quantidade.value = "";
+
+
+    var excluirItem = document.querySelectorAll(".excluir");
+    var itemLista = document.querySelectorAll('.lista-produto-single');
+    var valorExcluido = document.querySelectorAll('.price');
+    
+    let total = document.querySelector('.soma-produto h2 span');
+    total.innerHTML = soma
+    
+
+    excluirItem.forEach((botao, indice) => {
+        botao.addEventListener('click', () => {
+            itemLista[indice].remove();
+            itens.splice(indice,1)
+
+            total.innerHTML = total.innerHTML - valorExcluido[indice].innerHTML;
+            
+            console.log(itens);
+
+        })
+
+    })
+
+
+}
+
+function limpar() {
+    var mensagem;
+    var retorno = confirm('deseja limpar?');
+    if(retorno == true){
+    itens = [];
+    document.querySelector('.lista-produtos').innerHTML = "";
+    document.querySelector('.soma-produto h2 span').innerHTML = 0;
+    }else{
+        mensagem = alert("limpeza cancelada");
+    };
+}
+
+
+
